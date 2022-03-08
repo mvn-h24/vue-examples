97930a509c91daacb7920b2cfb5413719fae2f04
mvn-h24 <morning.vecher.night@yandex.ru>
2022-03-08 17:20:36 +0300

    add date-badge ui-lib component

---
+++ components/ui-lib/date-badge.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+    {{formattedDate}}
+    {{text}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  name: "DateBadge",
+  props: {
+    $date: {
+      type: Date,
+      required: true
+    },
+    text: {
+      type:String,
+      default: ()=> 'Date info'
+    }
+  },
+  computed: {
+    formattedDate(): string {
+      const d = this.$props.$date;
+      return `${d.getUTCHours()}:${d.getMinutes()}${d.getSeconds()}`;
+    }
+  }
+})
+</script>
